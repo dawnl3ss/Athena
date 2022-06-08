d01d2008f942af232f2053af7d8e9e6301224ab8
Neptune <52249849+Neptune-IT@users.noreply.github.com>
2022-06-08 22:31:32 +0200

    update [1.0.4] : fixing new_vector func

--- src/factory.h
+++ src/factory.h
@@ -1,8 +1,8 @@
 #ifndef ATHENA_FACTORY_H
 #define ATHENA_FACTORY_H
 
-#include "geometry/vector/vector.h"
+#include "geometry/vector/vector.cpp"
 
-vector new_vector(int x, int y);
+vector* new_vector(int x, int y);
 
 #endif
